96dc595e5d727ecbdfc1ac95cf815f3e55378825
AsReaderSupport <133012019+AsReaderSupport@users.noreply.github.com>
2023-06-29 18:04:38 -0400

    Delete ios-edge-driver directory

--- ios-edge-driver/ios/AsReaderGun/AsReaderGunSDK.framework/Headers/AsMaskActionType.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "AsRfidValues.h"
-
-@interface AsMaskActionType : NSObject
-+ (NSString *)toString:(MaskActionType)actionType targetType:(MaskTargetType)targetType;
-@end
--- ios-edge-driver/ios/AsReaderGun/AsReaderGunSDK.framework/Headers/AsProtocolAsReaderL251G.h
+++
@@ -1,6 +0,0 @@
-#import "AsProtocolAsReader.h"
-
-@interface AsProtocolAsReaderL251G: AsProtocolAsReader{
-}
-+ (instancetype)sharedInstanceInitWithAsReaderGUN:(AsReaderGUN *)asReaderGUN delegate:(id<AsReaderDelegate>)receiver;
-@end
